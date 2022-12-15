c250dcd857a5ff09809ade6ce259cef7c8345a7d
mark raphael <mark.raphael@bt.com>
2022-12-15 14:04:09 +0000

    WIP

--- __tests_/add.book.test.ts
+++ __tests_/add.book.test.ts
@@ -2,16 +2,19 @@ import {generatePgClientWithTestContainer} from "./test.container";
 
 jest.setTimeout(20000);
 
-test("Insert & Validate the names and prices of books in the PostgreSQL Container", async () => {
+let client;
+let resultset;
 
-    const client = await generatePgClientWithTestContainer();
-
+beforeAll(async () => {
+    // Seed (stage) values in the PostgreSQL Database
+    client = await generatePgClientWithTestContainer();
     await client.query("INSERT INTO books (name, price) VALUES ('Oliver Twist', 3.99), ('Jingle Jangle', 4.50);");
-    const resultset = await client.query("SELECT * from books");
+    resultset = await client.query("SELECT * from books");
+});
+
+test("Validate: Names and Prices of books in the PostgreSQL Container", async () => {
     expect(resultset.rows[0].price).toBe("3.99");
     expect(resultset.rows[1].price).toBe("4.50");
-
-    // console.log(resultset.fields);   // Displays the physical structure of the db in the container
 
     let i = 0;
     while (i < resultset.rowCount) {
@@ -19,7 +22,13 @@ test("Insert & Validate the names and prices of books in the PostgreSQL Containe
         console.log(resultset.rows[i].price);
         i++;
     }
-
-    await client.end();
-
 });
+
+test("Display the Physical Structure of the Database", async () => {
+    console.log(resultset.fields);
+});
+
+
+afterAll(async () => {
+    await client.end();
+})
